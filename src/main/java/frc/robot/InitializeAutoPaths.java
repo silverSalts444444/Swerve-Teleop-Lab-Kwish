d7631fe4f6db0056f4dc9e128ed9d96cfa55b14d
Thanurika <Thanurikaiyer@gmail.com>
2025-02-16 16:01:27 -0800

    Push before merge

--- src/main/java/frc/robot/InitializeAutoPaths.java
+++ src/main/java/frc/robot/InitializeAutoPaths.java
@@ -20,6 +20,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.swerve.SwerveDriveTrain;
+import frc.robot.commands.autoPaths.CaseResetPose;
 import frc.robot.commands.autoPaths.IyerTCase;
 import frc.robot.commands.autoPaths.TestCase215;
 import frc.robot.commands.autoPaths.TestPath;
@@ -34,6 +35,7 @@ public class InitializeAutoPaths {
     private TestPath testPath;
     private TestCase215 testCase215;
     private IyerTCase iyerCase;
+    private CaseResetPose poseResetCase;
 
     
     RobotConfig config;
@@ -75,9 +77,13 @@ public class InitializeAutoPaths {
           testPath = new TestPath(this.swerve);
           testCase215 = new TestCase215(this.swerve);
           iyerCase = new IyerTCase(this.swerve);
+          poseResetCase = new CaseResetPose(this.swerve);
+
           autoCommandChooser.setDefaultOption("Test Path", testPath);
           autoCommandChooser.addOption("Test Case 215", testCase215);
           autoCommandChooser.addOption("IyerT Case", iyerCase);
+          autoCommandChooser.addOption("CaseResetPose", poseResetCase);
+
           SmartDashboard.putData(autoCommandChooser);
 
         } catch (FileVersionException | IOException | ParseException e) {
