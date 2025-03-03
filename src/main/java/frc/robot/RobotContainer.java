c0ec9682f87c34935fd8268a71e682c0353d3a87
Yodaman07 <ayaan.irshad@gmail.com>
2025-03-02 17:25:36 -0800

    Removed unneeded comments

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -90,24 +90,10 @@ public class RobotContainer {
     mechJoystick.button(18).whileTrue(coralManipulator.releaseCoral()).onFalse(coralManipulator.stopCoral());
     
     
-    // mechJoystick.button(7).onTrue(coralManipulator.pivotIntake()); // TEMP
-    // mechJoystick.button(8).onTrue(coralManipulator.pivotPlace()); //TEMP
-    // mechController.a().onTrue(coralManipulator.pivotIntake());
     mechController.b().onTrue(coralManipulator.pivotPreset());           
-    // mechController.x().onTrue(coralManipulator.pivotL4());      
-    // mechJoystick.button(8).onTrue(coralManipulator.pivotPlace());
     
-    
-    // mechController.axisGreaterThan(1, 0.1).whileTrue(coralManipulator.movePivot()); 
-    // mechController.axisLessThan(1, -0.1).whileTrue(coralManipulator.movePivot()); 
 
     mechJoystick.axisMagnitudeGreaterThan(7, 0.1).whileTrue(coralManipulator.movePivot());
-    
-
-    // Trigger coralStopB1 = mechController.axisLessThan(5, 0.1);
-    // Trigger coralStopB2 = mechController.axisGreaterThan(5, -0.1);
-    
-    // coralStopB1.and(coralStopB2).onTrue(coralManipulator.pivotStop()); 
     
   }
 
@@ -119,19 +105,12 @@ public class RobotContainer {
     Homing home = new Homing(elevator);
     mechJoystick.axisMagnitudeGreaterThan(5, 0.1).whileTrue(elevator.moveElevator());
     
-    // mechJoystick.button(0).onTrue(elevator.stallElevator()); //TEMPORARY
+    
     mechJoystick.button(1).onTrue(elevator.setHeightL4());
     mechJoystick.button(2).onTrue(elevator.setHeightL3());
     mechJoystick.button(3).onTrue(elevator.setHeightL2());
 
-    // mechController.a().onTrue(elevator.setHeightL2()); // ALL TEMP
-    // mechController.b().onTrue(elevator.setHeightL3());
-    // mechController.x().onTrue(elevator.setHeightL4());
-    
-    
-
     mechJoystick.button(6).onTrue(home); 
-    // mechJoystick.button(0).onTrue(elevator.stopElevator()); //TEMP
   }
 
   public Command getAutonomousCommand() {
